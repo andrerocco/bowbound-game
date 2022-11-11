499d5482afb757c7de9efed91fa0c59399c806ac
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2022-11-11 09:38:09 -0300

    Correção de indentação

--- Game/data/abstractSingleton.py
+++ Game/data/abstractSingleton.py
@@ -2,6 +2,6 @@ class Singleton(object):
 		__instance = None
 
 		def __new__(cls, *args):
-				if cls.__instance is None:
-						cls.__instance = object.__new__(cls, *args)
-				return cls.__instance
+			if cls.__instance is None:
+				cls.__instance = object.__new__(cls, *args)
+			return cls.__instance
