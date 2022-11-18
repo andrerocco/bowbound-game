0b4d8b77ca91fc9196386b835bc5b4416abf3726
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2022-11-18 09:24:12 -0300

    Criada base de classe LevelPlaying

--- Game/data/states/stateLevelPlaying.py
+++ Game/data/states/stateLevelPlaying.py
@@ -1,1 +1,23 @@
 from states.abstractState import State
+
+
+class LevelPlaying(State):
+    def __init__(self, game):
+        super().__init__(game)
+        
+
+
+    def update(self):
+        ...
+
+    def render(self):
+        ...
+
+    def set_level_score(self):
+        ...
+
+    def next_level(self):
+        ...
+
+    def restart_level(self):
+        ...
